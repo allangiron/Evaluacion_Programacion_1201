946db3b84c3c5fe11f8ef2e72e6558d5fcbc5159
Allan Giron <86334962+08011498@users.noreply.github.com>
2021-07-02 08:04:49 -0600

    Segundo Commit
    Avance de la Evaluacion 2

--- Evaluacion_Programacion_1201/Form1.cs
+++ Evaluacion_Programacion_1201/Form1.cs
@@ -16,5 +16,27 @@ namespace Evaluacion_Programacion_1201
         {
             InitializeComponent();
         }
+
+        private void Btn_obtenerPromedio_Click(object sender, EventArgs e)
+        {
+            Promedio estudiante = new Promedio();
+            estudiante.Nombre = Txt_nombre.Text;
+            estudiante.Cuenta = Convert.ToInt32(Txt_cuenta.Text);
+            estudiante.Nota1 = Convert.ToInt32(Txt_nota1.Text);
+            estudiante.Nota2 = Convert.ToInt32(Txt_nota2.Text);
+            estudiante.Nota3 = Convert.ToInt32(Txt_nota3.Text);
+            estudiante.Nota4 = Convert.ToInt32(Txt_nota4.Text);
+
+            MessageBox.Show("Nombre: " + estudiante.Nombre);
+            MessageBox.Show("El Numero De Cuenta Es: " + estudiante.Cuenta);
+            MessageBox.Show("Promedio: " + estudiante.Promedio1);
+            MessageBox.Show(estudiante.Iteracion);
+
+        }
+
+        private void Btn_salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
--- Evaluacion_Programacion_1201/Promedio.cs
+++ Evaluacion_Programacion_1201/Promedio.cs
@@ -8,5 +8,46 @@ namespace Evaluacion_Programacion_1201
 {
     class Promedio
     {
+        private string nombre;
+        private int cuenta;
+        private int nota1;
+        private int nota2;
+        private int nota3;
+        private int nota4;
+        private int promedio;
+        private string iteracion;
+
+        public String Nombre { get; set; }
+        public int Cuenta { get; set; }
+        public int Nota1 { get; set; }
+        public int Nota2 { get; set; }
+        public int Nota3 { get; set; }
+        public int Nota4 { get; set; }
+
+        public int Promedio1
+        {
+            get { promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+                  return promedio;
+            }
+            set { promedio = value; }
+        }
+
+        public string Iteracion
+        {
+            get
+            {
+                if (promedio >= 70)
+                {
+                    iteracion = " Felicidades Usted Aprobo La Clase ";
+                }
+
+                if (promedio < 70)
+                {
+                    iteracion = " Lo Sentimos, Ha Reprobado La Clase ";
+                }
+                return iteracion;
+            }
+        }
+
     }
 }
